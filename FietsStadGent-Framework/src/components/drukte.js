eb4f095379d20311a0cdde170c8e9cf07c35ae96
Lukas Boon <lukas.boon@ugent.be>
2026-04-28 20:43:41 +0200

    season colors

--- FietsStadGent-Framework/src/components/drukte.js
+++ FietsStadGent-Framework/src/components/drukte.js
@@ -2,12 +2,29 @@ import * as Plot from "npm:@observablehq/plot";
 import * as d3 from "d3";
 
 export function drukte(data, yLabel, {width, height} = {}) {
+  function getSeason(month) {
+    const m = month.getMonth();
+    if (m >= 2 && m <= 4) return 'spring';
+    if (m >= 5 && m <= 7) return 'summer';
+    if (m >= 8 && m <= 10) return 'autumn';
+    return 'winter';
+  }
+
+  const seasonColors = {
+    spring: 'green',
+    summer: 'yellow',
+    autumn: 'orange',
+    winter: 'blue'
+  };
+
   return Plot.plot({
     width,
     height,
     marginTop: 30,
     color: {
-      legend: false
+      domain: ['lente', 'zomer', 'herfst', 'winter'],
+      range: ['green', 'yellow', 'orange', 'blue'],
+      legend: true,
     },
     x: {
       label: "Datum",
@@ -23,7 +40,7 @@ export function drukte(data, yLabel, {width, height} = {}) {
       Plot.barY(data, {
         x: "month",
         y: "avg",
-        fill: "steelblue",
+        fill: d => seasonColors[getSeason(d.month)],
       })
     ]
   });
